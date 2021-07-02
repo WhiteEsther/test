16908ed418ec8b2bd4be7fd3af4c34ea793274aa
WhiteEsther <1986200733@qq.com>
2021-07-02 10:26:51 +0800

    第一次提交

---
+++ src/HolloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: Esther
+ * @Description: //TODO
+ * @Date: 2021/7/2 10:23
+ * @Version: 1.0
+ */
+public class HolloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hollo world!");
+    }
+}
